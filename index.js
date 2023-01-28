90caa7763c0399165a626c71c31f8aed8090cb32
Mohammed Al-jaberi <aljabri606@gmail.com>
2023-01-28 12:06:29 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution here!
+
+let cats = [] ; 
+cats.push('Milo', 'Otis', 'Garfield');
+cats.push('Ralph');
+cats.unshift("Bob");
+console.log(cats);
+cats.pop('Ralph') ; 
+cats.shift("Bob");
+let appendCat = [...cats , "Broom"];
+let prependCat = ["Arnold",...cats] ; 
+cats.slice(2); 
+cats.slice(0);
+
+
+
+
+
